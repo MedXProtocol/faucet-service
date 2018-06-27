2b58af4f5ab4d5677243585eaa5432a1e024bf10
Charles Bergeron <chuck.bergeron@gmail.com>
2018-06-27 14:46:51 -0700

    * rename constants

--- docker/express-server/send-signed-transaction-service.js
+++ docker/express-server/send-signed-transaction-service.js
@@ -5,16 +5,15 @@ const web3 = new Web3(new Web3.providers.HttpProvider(process.env.FAUCET_CONFIG_
 
 // requires leading '0x' ! not the pure metamask exported key
 const PRIVATE_KEY = process.env.FAUCET_CONFIG_PRIVKEY;
-const contractOwnerAddress = '0x09c0048e162455b981a6caa2815469dfea18759d';
+const CONTRACT_OWNER_ADDRESS = '0x09c0048e162455b981a6caa2815469dfea18759d';
 
-const contractAddress = process.env.FAUCET_CONFIG_TOKEN_CONTRACT_ADDRESS;
-const recipientAddress = '0xc5dc4aadf45a8c8cfa5460f2a14175c45ac8528d';
+const CONTRACT_ADDRESS = process.env.FAUCET_CONFIG_TOKEN_CONTRACT_ADDRESS;
 
-const tokenAmount = "1000"
+const TOKEN_AMOUNT = "1000"
 
-const mintFunctionName = 'mint'
+const MINT_FUNCTION_NAME = 'mint'
 
-const mintFunctionSignature = [{
+const MINT_FUNCTION_INPUTS = [{
     type: 'address',
     name: '_to'
   },{
@@ -23,36 +22,49 @@ const mintFunctionSignature = [{
   }]
 
 
-function encodeFunctionCall(recipientAddress, tokenAmount) {
+function encodeFunctionCall(recipientAddress) {
   return web3.eth.abi.encodeFunctionCall({
-    name: mintFunctionName,
+    name: MINT_FUNCTION_NAME,
     type: 'function',
-    inputs: mintFunctionSignature
+    inputs: MINT_FUNCTION_INPUTS
   }, [
     recipientAddress,
-    web3.utils.toHex(tokenAmount)
+    web3.utils.toHex(TOKEN_AMOUNT)
   ]);
 }
 
 
 // web3.eth.estimateGas({
-//      "from"      : contractOwnerAddress,
-//      "to"        : contractAddress,
+//      "from"      : CONTRACT_OWNER_ADDRESS,
+//      "to"        : CONTRACT_ADDRESS,
 //      "data"      : txData
 // }).then(console.log)
 
-function buildContractTxObject(recipientAddress, tokenAmount) {
-  let data = encodeFunctionCall(recipientAddress, tokenAmount)
+function buildEtherTxObject(recipientAddress) {
+  const txObject = {
+    to: recipientAddress,
+    value: web3.utils.toHex("1000"), // adding value to send to contract throws out of gas error
+    gas: web3.utils.toHex(21000), // 21000 is expected gas for an ETH value transfer
+    gasPrice: web3.utils.toHex(20), // 20
+    from: CONTRACT_OWNER_ADDRESS
+  }
+  console.log('buildEtherTxObject: ' + JSON.stringify(txObject, null, 4));
+
+  return txObject
+}
+
+function buildContractTxObject(recipientAddress) {
+  let data = encodeFunctionCall(recipientAddress)
   console.log(data)
   const txObject = {
-    to: contractAddress,
+    to: CONTRACT_ADDRESS,
     // value: web3.utils.toHex("1000"), // adding value to send to contract throws out of gas error
     data: data,
-    gas: web3.utils.toHex(202114), // 102114
-    gasPrice: web3.utils.toHex(20), // 20
-    from: contractOwnerAddress
+    gas: web3.utils.toHex(122114), // 102114
+    gasPrice: web3.utils.toHex(90), // 20
+    from: CONTRACT_OWNER_ADDRESS
   }
-  console.log('built txObject: ' + txObject.toString())
+  console.log('built txObject: ' + JSON.stringify(txObject, null, 4));
 
   return txObject
 }
@@ -69,8 +81,6 @@ async function signTransaction(txObject) {
       console.error(error);
     });
 
-  console.log('rawTx is: ' + rawTx);
-
   return rawTx
 }
 
@@ -80,35 +90,40 @@ async function sendSignedTransaction(rawTx) {
       console.log("txHash is: " + txHash)
       return txHash
     })
-    .on('error', console.error)
-    .on('receipt', function(a) {console.log(a)})
-    .on('confirmation', function(a) {console.log(a)})
+    // .on('receipt', function(a) {console.log(a)})
+    // .on('confirmation', function(a) {console.log(a)})
     .catch(error => {
       console.error(error);
     });
-
 
   console.log('promiEvent is: ' + promiEvent);
 
   return promiEvent;
 }
 
+// Sending data to a contract's function
 let sendSignedTokenTransaction = async function(ethAddress) {
-  const amount = 1000
-
-  const txObject = buildContractTxObject(ethAddress, amount);
+  const txObject = buildContractTxObject(ethAddress);
   const rawTx = await signTransaction(txObject)
   const promiEvent = await sendSignedTransaction(rawTx);
 
   return promiEvent;
 }
 
+// Sending pure value of ether
+let sendEtherTransaction = async function(ethAddress) {
+  const txObject = buildEtherTxObject(ethAddress);
+  const rawTx = await signTransaction(txObject)
+  const promiEvent = await sendSignedTransaction(rawTx);
+
+  return promiEvent;
+}
+
+const recipientAddress = '0xc5dc4aadf45a8c8cfa5460f2a14175c45ac8528d';
 sendSignedTokenTransaction(recipientAddress)
+// sendEtherTransaction(recipientAddress)
 
 module.exports = {
-  sendEther: function(ethAddress) {
-    return "HELLO";
-  },
-
+  sendEther: sendEtherTransaction,
   sendToken: sendSignedTokenTransaction
 };
